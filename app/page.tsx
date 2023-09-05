0227521dcc5a75e5ee6479f43352fdfe43f65725
moonhopark <moonho4199@naver.com>
2023-09-05 18:28:35 +0900

    feat: fetching data

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1>Home Page</h1>
       <Link href="/about">Go to About Page</Link>
+      <Link href="/users">Users</Link>
     </main>
   );
 }
